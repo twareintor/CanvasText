2b1a1ffe6ac7fd65088fbde71566b910231ec426
Claudiu Ciutacu <ciutacu@hotmail.com>
2019-06-14 08:34:13 +0200

    As-Built

---
+++ canvas.h
@@ -0,0 +1,13 @@
+#include  <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void makeCanvas(char*** ppcanvas, int width, int height);
+
+void printCanvas(char** pcanvas, int width, int height);
+
+void deleteCanvas(char*** ppcanvas, int width, int height);
+
+void test_draw(char*** canvas);
+
+void test_draw2(char*** ppcanvas);
